519cbbd0afe23215601ad75fb5d949d32e8972f4
hide-yoshi <hide-yoshi@users.noreply.github.com>
2024-10-09 13:30:13 +0900

    update sample

--- src/relayer_backend/src/lib.rs
+++ src/relayer_backend/src/lib.rs
@@ -1,37 +1,96 @@
+use std::str::FromStr;
+
+use serde::Serialize;
 use solana_client_icp::{
-    solana_sdk::signer::{
-        threshold_signer::{SchnorrKeyIds, ThresholdSigner},
-        Signer,
+    solana_sdk::{
+        instruction::Instruction,
+        native_token::LAMPORTS_PER_SOL,
+        signer::{
+            threshold_signer::{SchnorrKeyIds, ThresholdSigner},
+            Signer,
+        },
+        transaction::Transaction,
     },
     WasmClient,
 };
-
-thread_local! {
-    static DESTINATION : std::cell::RefCell<Option<String>> = std::cell::RefCell::new(None);
-}
+use solana_program::{instruction::AccountMeta, pubkey::Pubkey, system_program};
+const DESTINATION: &str = "9z3hM1vW44tw2uPh99Jh59xzEHjVaRoNzj6Nymcuv86V";
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
 #[ic_cdk::update]
-fn set_program_destination(destination: String) {
-    DESTINATION.with(|d| {
-        *d.borrow_mut() = Some(destination);
-    });
-}
-
-#[ic_cdk::query]
-fn get_program_destination() -> String {
-    DESTINATION.with(|d| d.borrow().clone().unwrap_or_else(|| "".to_string()))
+async fn sample() {
+    let balance = devnet_client()
+        .get_balance(&local_signer().await.pubkey())
+        .await
+        .unwrap();
+    ic_cdk::print(format!("Balance: {}", balance).as_str());
 }
 
 #[ic_cdk::update]
-async fn sample() {
-    let client = WasmClient::new("https://api.devnet.solana.com");
-    let signer = ThresholdSigner::new(SchnorrKeyIds::TestKeyLocalDevelopment)
+async fn sample_airdrop() {
+    let signer = local_signer().await;
+    let client = devnet_client();
+    let before_balance = client.get_balance(&signer.pubkey()).await.unwrap();
+    let result = client
+        .request_airdrop(&signer.pubkey(), LAMPORTS_PER_SOL)
+        .await;
+    match result {
+        Ok(tx) => ic_cdk::print(format!("Airdrop successful: {:?}", tx).as_str()),
+        Err(e) => ic_cdk::print(format!("Airdrop failed: {:?}", e).as_str()),
+    }
+    let after_balance = client.get_balance(&signer.pubkey()).await.unwrap();
+    ic_cdk::println!(
+        "Balance before: {}, after: {}",
+        before_balance,
+        after_balance
+    );
+}
+
+#[derive(Serialize)]
+pub struct Initialize {
+    pub key: Vec<u8>,
+}
+
+#[ic_cdk::update]
+async fn sample_instruction() {
+    let program = Pubkey::from_str(DESTINATION).unwrap();
+    let signer = local_signer().await;
+    let client = devnet_client();
+    let key = "BTC".to_string();
+    let key_utf8 = key.as_bytes().as_ref();
+    let initialize_discriminant: [u8; 8] = [175, 175, 109, 31, 13, 152, 155, 237];
+    let pda = Pubkey::find_program_address(&vec![key_utf8], &program);
+    let instruction = Instruction::new_with_borsh(
+        program,
+        &(initialize_discriminant, "BTC".to_string()),
+        vec![
+            AccountMeta::new(pda.0, false),
+            AccountMeta::new(signer.pubkey(), true),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+    );
+    let signers: Vec<Box<dyn Signer>> = vec![Box::new(signer.clone())];
+    let block_hash = client.get_latest_blockhash().await.unwrap();
+    let tx = Transaction::new_signed_with_payer(
+        &vec![instruction],
+        Some(&signer.pubkey()),
+        &signers,
+        block_hash,
+    )
+    .await;
+    let result = client.send_transaction(&tx).await;
+    ic_cdk::println!("Transaction result: {:?}", result);
+}
+
+fn devnet_client() -> WasmClient {
+    WasmClient::new("https://api.devnet.solana.com")
+}
+
+async fn local_signer() -> ThresholdSigner {
+    ThresholdSigner::new(SchnorrKeyIds::TestKeyLocalDevelopment)
         .await
-        .unwrap();
-    let balance = client.get_balance(&signer.pubkey()).await.unwrap();
-    ic_cdk::print(format!("Balance: {}", balance).as_str());
+        .unwrap()
 }
