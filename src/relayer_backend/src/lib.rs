607fc3225d856ef40bc3dde5ea88171d0385e07f
hide-yoshi <hide-yoshi@users.noreply.github.com>
2024-10-09 14:20:58 +0900

    refactor

--- src/relayer_backend/src/lib.rs
+++ src/relayer_backend/src/lib.rs
@@ -1,6 +1,5 @@
 use std::str::FromStr;
 
-use serde::Serialize;
 use solana_client_icp::{
     solana_sdk::{
         instruction::Instruction,
@@ -11,7 +10,7 @@ use solana_client_icp::{
         },
         transaction::Transaction,
     },
-    WasmClient,
+    CallOptions, WasmClient,
 };
 use solana_program::{instruction::AccountMeta, pubkey::Pubkey, system_program};
 const DESTINATION: &str = "9z3hM1vW44tw2uPh99Jh59xzEHjVaRoNzj6Nymcuv86V";
@@ -23,7 +22,7 @@ fn greet(name: String) -> String {
 #[ic_cdk::update]
 async fn sample() {
     let balance = devnet_client()
-        .get_balance(&local_signer().await.pubkey())
+        .get_balance(&local_signer().await.pubkey(), CallOptions::default())
         .await
         .unwrap();
     ic_cdk::print(format!("Balance: {}", balance).as_str());
@@ -33,15 +32,21 @@ async fn sample() {
 async fn sample_airdrop() {
     let signer = local_signer().await;
     let client = devnet_client();
-    let before_balance = client.get_balance(&signer.pubkey()).await.unwrap();
+    let before_balance = client
+        .get_balance(&signer.pubkey(), CallOptions::default())
+        .await
+        .unwrap();
     let result = client
-        .request_airdrop(&signer.pubkey(), LAMPORTS_PER_SOL)
+        .request_airdrop(&signer.pubkey(), LAMPORTS_PER_SOL, CallOptions::default())
         .await;
     match result {
         Ok(tx) => ic_cdk::print(format!("Airdrop successful: {:?}", tx).as_str()),
         Err(e) => ic_cdk::print(format!("Airdrop failed: {:?}", e).as_str()),
     }
-    let after_balance = client.get_balance(&signer.pubkey()).await.unwrap();
+    let after_balance = client
+        .get_balance(&signer.pubkey(), CallOptions::default())
+        .await
+        .unwrap();
     ic_cdk::println!(
         "Balance before: {}, after: {}",
         before_balance,
@@ -49,9 +54,9 @@ async fn sample_airdrop() {
     );
 }
 
-#[derive(Serialize)]
-pub struct Initialize {
-    pub key: Vec<u8>,
+#[ic_cdk::update]
+async fn get_pubkey() -> String {
+    local_signer().await.pubkey().to_string()
 }
 
 #[ic_cdk::update]
@@ -59,13 +64,13 @@ async fn sample_instruction() {
     let program = Pubkey::from_str(DESTINATION).unwrap();
     let signer = local_signer().await;
     let client = devnet_client();
-    let key = "BTC".to_string();
+    let key = "TEST2".to_string();
     let key_utf8 = key.as_bytes().as_ref();
     let initialize_discriminant: [u8; 8] = [175, 175, 109, 31, 13, 152, 155, 237];
     let pda = Pubkey::find_program_address(&vec![key_utf8], &program);
     let instruction = Instruction::new_with_borsh(
         program,
-        &(initialize_discriminant, "BTC".to_string()),
+        &(initialize_discriminant, "TEST2".to_string()),
         vec![
             AccountMeta::new(pda.0, false),
             AccountMeta::new(signer.pubkey(), true),
@@ -73,7 +78,10 @@ async fn sample_instruction() {
         ],
     );
     let signers: Vec<Box<dyn Signer>> = vec![Box::new(signer.clone())];
-    let block_hash = client.get_latest_blockhash().await.unwrap();
+    let block_hash = client
+        .get_latest_blockhash(CallOptions::default())
+        .await
+        .unwrap();
     let tx = Transaction::new_signed_with_payer(
         &vec![instruction],
         Some(&signer.pubkey()),
@@ -81,7 +89,7 @@ async fn sample_instruction() {
         block_hash,
     )
     .await;
-    let result = client.send_transaction(&tx).await;
+    let result = client.send_transaction(&tx, CallOptions::default()).await;
     ic_cdk::println!("Transaction result: {:?}", result);
 }
 
